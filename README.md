b1423f65735e92464eae1807c6f5e5b1b9f268d0
Hal <haln_01@proton.me>
2022-09-12 22:37:32 -0700

    updated README

--- README.md
+++ README.md
@@ -2,14 +2,6 @@
 
 Server for a reddit client, this client repo is hosted [here](https://github.com/Chuck-Gibson/reddit-app).
 
-## IMPORTANT
-
-Currently we are not implementing `OAuth2` authentication, though we will
-be expanding the project and add this feature in a future date.
-
-Some API endpoints are set up for just frontend stuff at the moment,
-but I will update this README as we go along.
-
 ## API for posts
 
 Raw API response from Reddit has a lot of information that would not be needed
--- src/server.js
+++ src/server.js
@@ -3,13 +3,12 @@ const cors = require('cors');
 const postsRoute = require('./routes/redditPosts');
 
 const PORT = process.env.PORT || 3001;
-const CLIENT_URL = 'http://localhost:3000';
-
+const CLIENT_URL = 'http://localhost:3000'; // Change this once frontend app is deployed
 
 // Initialize express
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // CORS
 app.use(cors({ origin: CLIENT_URL }));
 
@@ -21,11 +20,10 @@ app.get('/', (req, res) => {
 // handle `top`, `hot`, `rising`, `new`
 app.use('/posts', postsRoute);
 
-
 // Error handling
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const errorMessage = err.message || "Something went wrong in the server";
+  const errorMessage = err.message || 'Something went wrong in the server';
   res.status(status).json({ message: errorMessage });
-})
+});
 app.listen(PORT, () => console.log(`server live on port ${PORT}`));
