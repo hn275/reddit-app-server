5f7eb2ab9d8fbebe32aa219312482cd0a1059cb5
Hal <haln_01@proton.me>
2022-09-15 23:10:12 -0700

    added props to url

--- src/routes/links/util/parseLink.js
+++ src/routes/links/util/parseLink.js
@@ -14,6 +14,22 @@ const parseMedia = (mediaObject) => {
   }
 };
 
+const parseUrl = (urlObject) => {
+  /**
+   * This function parse incoming url to determine if it's an image or
+   * just an external link to external content
+   * @params {string} urlObject: url to a reddit source, external or internal
+   * return: if urlObject is not supplied then `null`, else it's a nested object with props
+   */
+  if (!urlObject) return null;
+
+  const imgReg = /\.(jpg|jpeg|png|gif)$/;
+  return {
+    isImage: imgReg.test(urlObject),
+    contentUrl: urlObject,
+  };
+};
+
 const parseResponse = (responseJson) => {
   /**
    * @params {object} responseJson: json object received from get request
@@ -21,16 +37,15 @@ const parseResponse = (responseJson) => {
    * (check getAPI.md in docs)
    */
   const responsePosts = responseJson.data.children;
-  // console.log(responsePosts)
   const allParsedPost = responsePosts.map((postInfo) => {
-    const post = postInfo.data;
+    const post = postInfo.data; // data per post
     return {
       type: 't3',
       title: post.title,
       subreddit: post.subreddit_name_prefixed,
       voteScore: post.score,
       id: post.id,
-      url: post.url || null,
+      url: parseUrl(post.url_overridden_by_dest),
       author: post.author,
       commentCount: post.num_comments,
       isVideo: post.is_video,
