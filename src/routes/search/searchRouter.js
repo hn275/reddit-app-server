2dbc14eff6b21c2c0160a4c884558c6befa126c1
Hal <haln_01@proton.me>
2022-09-21 01:17:37 -0700

    added searchRouter

--- src/routes/search/searchRouter.js
+++ src/routes/search/searchRouter.js
@@ -1,29 +1,37 @@
 const searchRouter = require('express').Router();
 const axios = require('axios');
 const getWhiteSpace = require('./utils/getWhiteSpace');
+const getSearchResults = require('./utils/getSearchResults');
 const URL = require('../url');
+const getSearchType = require('./utils/getSearchType');
 
 searchRouter.get('/', async (req, res, next) => {
   try {
     // sanitize search string
-    const search = req.body.search;
-    if (!search) {
-      const error = new Error('Missing search query');
+    const { search, type } = req.body;
+    if (!search || !type) {
+      const error = new Error(
+        'Missing search query, required `search` and `searchType`'
+      );
       error.status = 400;
       return next(error);
     }
 
+    const searchParsed = getWhiteSpace(search);
+    const typeParsed = getSearchType(type);
+
     let fetchingUrl = URL;
     fetchingUrl += '/search.json';
-    fetchingUrl += `?q=${getWhiteSpace(search)}`;
+    fetchingUrl += `?q=${searchParsed}`;
     fetchingUrl += `&limit=15`;
     fetchingUrl += '&sort=top';
     fetchingUrl += '&t=week';
 
     const redditResponse = await axios.get(fetchingUrl);
-    res.json(redditResponse.data);
-  } catch (e) {
-    next(e);
+    const redditData = redditResponse.data.data.children;
+    res.json(getSearchResults(redditData));
+  } catch (error) {
+    return next(error);
   }
 });
 
